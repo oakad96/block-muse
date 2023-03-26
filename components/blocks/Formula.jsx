1e8b1e07d584d19b7e7272aab5a11e65ab0036ff
Onur Akad <is.onurakad@gmail.com>
2023-03-26 07:40:42 +0300

    Dummy Formula Block

--- components/blocks/Formula.jsx
+++ components/blocks/Formula.jsx
@@ -1,5 +1,22 @@
+// components/blocks/Formula.js
 import React from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
 
-export const Formula = () => {
-  return <div>Formula</div>;
+const Formula = ({ formula, result }) => {
+  return (
+    <Box my={4}>
+      <Input
+        placeholder="Enter formula"
+        value={formula || ""}
+        isReadOnly
+        mb={2}
+        borderColor="gray.400"
+      />
+      <Text fontSize="md" fontWeight="bold">
+        Result: {result || "No result"}
+      </Text>
+    </Box>
+  );
 };
+
+export default Formula;
